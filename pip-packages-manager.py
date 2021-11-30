30586e2a1eb9b535143f4f2b80bfb72d735a4e05
Nucky <xinghuaizhen1989@gmail.com>
2021-11-30 22:30:39 +0800

    v0.0.4 - 更改输出风格，与pip freeze风格保持一致

--- pip-packages-manager.py
+++ pip-packages-manager.py
@@ -2,43 +2,21 @@
 
 import os
 import json
-import sys, getopt
+import sys
 
-def get_file_name(argv):
+def get_packages_info(args):
+    if args == None:
+        package_json = os.popen("pipdeptree --json-tree").read()
+        package_json = json.loads(package_json)
+        packages_info = package_json
 
-    if argv == None:
-        return None
-    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    except getopt.GetoptError:
-      print('usage: python3 test.py -o requirements.txt')
-      sys.exit(2)
-    for opt, arg in opts:
-      if opt == "-o":
-         return arg
-      else:
-          print('usage: python3 test.py -o requirements.txt')
-          sys.exit()
-
-def get_packages_info(file_name=None):
-    package_json = os.popen("pipdeptree --json-tree").read()
-    package_json = json.loads(package_json)
-    packages_info = package_json
-
-    for packages in packages_info:
-        package_name = packages.get("package_name")
-        package_version = packages.get("installed_version")
-        if file_name:
-            with open(file_name, "a") as f:
-                f.write(f"{package_name}=={package_version}\n")
-        else:
+        for packages in packages_info:
+            package_name = packages.get("package_name")
+            package_version = packages.get("installed_version")
             print(f"{package_name}=={package_version}")
-
-
-def main():
-    file_name = get_file_name(sys.argv[1:])
-    get_packages_info(file_name)
+    else:
+        print('usage: python3 pip-packages-manager.py >> your_requirements.txt')
 
 if __name__ == "__main__":
-   main()
+   get_packages_info(sys[1:])
 
