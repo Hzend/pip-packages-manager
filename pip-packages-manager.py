60e67da66b76c6c18a9b48433ea6f42a5160a294
Nucky <xinghuaizhen1989@gmail.com>
2021-11-30 22:16:58 +0800

    v0.0.1 - 完善选项，选项为空则打印依赖

--- pip-packages-manager.py
+++ pip-packages-manager.py
@@ -6,6 +6,9 @@ import sys, getopt
 
 def get_file_name(argv):
 
+    if argv == None:
+
+
     try:
       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
     except getopt.GetoptError:
@@ -18,9 +21,7 @@ def get_file_name(argv):
           print('usage: python3 test.py -o requirements.txt')
           sys.exit()
 
-def get_packages_info(file_name):
-    os.environ["PATH"] = "/home/project/pip-package-manager/bin"
-
+def get_packages_info(file_name=None):
     package_json = os.popen("pipdeptree --json-tree").read()
     package_json = json.loads(package_json)
     packages_info = package_json
@@ -28,8 +29,11 @@ def get_packages_info(file_name):
     for packages in packages_info:
         package_name = packages.get("package_name")
         package_version = packages.get("installed_version")
-        with open(file_name, "a") as f:
-            f.write(f"{package_name}=={package_version}\n")
+        if file_name:
+            with open(file_name, "a") as f:
+                f.write(f"{package_name}=={package_version}\n")
+        else:
+            print(f"{package_name}=={package_version}")
 
 
 def main():
